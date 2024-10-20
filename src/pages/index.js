cc376ff187622b598863480b08aefa95dcb0afd3
Your Name <duyiqundean@gmail.com>
2024-10-20 00:36:54 -0700

    I tried my best but the open banking api does not even give cors access

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Dashboard } from './Dashboard';
 export { default as LandingPage } from './LandingPage';
 export { default as Login } from './Login';
 export { default as Register } from './Register';
+export {default as Chatbox} from './AIconv'
